f6a6e8a818b2af679060ed5580b7e6623df7dec6
Ary Baldioceda <abaldioceda@gmail.com>
2017-11-16 02:11:46 +0000

    added delete and edit routs and views

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -49,4 +49,52 @@ class ProjectController < ApplicationController
     end
   end
 
+  get '/projects/:id/edit' do
+    if logged_in?
+      @project = Project.find(params[:id])
+      erb :"projects/edit"
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/projects/:id' do
+    if logged_in? && current_user
+      if params[:name] != "" && params[:category] != ""
+        project = Project.find(params[:id])
+        category = Category.find_or_create_by(name: params[:category])
+        project.update(params[:project])
+        project.category = category
+        project.ideas.clear
+        params[:ideas].each do |idea|
+          project.ideas.build(text: idea)
+        end
+        project.save
+        redirect "/projects/#{project.id}"
+      else
+        redirect "/projects/#{params[:id]}/edit"
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  delete  '/projects/:id/delete' do
+    project = Project.find(params[:id])
+    user = current_user
+    if user.projects.include?(project)
+      project.delete
+      redirect '/projects'
+    else
+      redirect "/projects"
+    end
+  end
+
+
+
+
+
+
+
+
 end
