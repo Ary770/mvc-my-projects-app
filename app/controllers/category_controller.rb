4b0c5c50b110b4016e2fa55bed3a2d139adc1460
Ary Baldioceda <abaldioceda@gmail.com>
2017-11-17 02:36:55 +0000

    accounted for category duplication on controller and views

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,7 +2,7 @@ class CategoryController < ApplicationController
 
   get '/categories' do
     if logged_in?
-      # binding.pry
+      @categories = current_user.categories.uniq
       erb :'categories/categories'
     else
       redirect to '/login'
@@ -11,40 +11,11 @@ class CategoryController < ApplicationController
 
   get '/categories/:id' do
     if logged_in?
-      @category = Category.find(params[:id])
+      @category = current_user.categories.find(params[:id])
       erb :'categories/show_category'
     else
       redirect '/login'
     end
   end
-
-  # get '/projects/new' do
-  #   if logged_in?
-  #     erb :"projects/create_project"
-  #   else
-  #     redirect '/login'
-  #   end
-  # end
-  #
-  # post '/projects/new' do
-  #   if logged_in? && current_user
-  #     if params[:name] != "" && params[:category]
-  #       user = current_user
-  #       category = Category.find_or_create_by(name: params[:category])
-  #       project = user.projects.build(params[:project])
-  #       project.category = category
-  #       params[:ideas].each do |idea|
-  #         project.ideas.build(text: idea)
-  #       end
-  #       user.save
-  #
-  #       redirect "/projects/#{project.id}"
-  #     else
-  #       redirect '/projects/new'
-  #     end
-  #   else
-  #     redirect '/login'
-  #   end
-  # end
 
 end
